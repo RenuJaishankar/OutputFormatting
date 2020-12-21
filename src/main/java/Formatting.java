f3efecab37d23401df1a818490740c9ebcb6fe41
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-12-21 08:51:42 -0500

    new commit

--- src/main/java/Formatting.java
+++ src/main/java/Formatting.java
@@ -7,6 +7,7 @@ public class Formatting {
            String s1=sc.next();
            int x=sc.nextInt();
            System.out.printf("%-15s%03d%n",s1,x);
+           System.out.println("Hello");
        }
 
        System.out.println("=======================");
