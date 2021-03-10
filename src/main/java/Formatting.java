99b146475a07828a487a91f955b3d3d84d30f731
Renu Jaishankar <Renu.Jaishankar@yahoo.com>
2021-03-09 20:13:52 -0500

    new commit

--- src/main/java/Formatting.java
+++ src/main/java/Formatting.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Formatting {
    public static void main(String[] args){
        Scanner sc = new Scanner(System.in);
-       System.out.println("=======================");
-       System.out.println("This program is an example for output formatting");
+       System.out.println("=========================");
+       System.out.println("This program is an example for output formatting  ");
        for(int i=0;i<3;i++){
            String s1=sc.next();
            int x=sc.nextInt();
@@ -11,7 +11,7 @@ public class Formatting {
            System.out.println("Hello");
        }
 
-       System.out.println("=======================");
+       System.out.println("*=======================");
    }
 
 }
